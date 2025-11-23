193f4ca8edc46f16201b91a6633905ba843b2f1e
Ewan Palmer <tpe.dev@proton.me>
2025-11-23 15:42:14 +0000

    updated global color scheme

--- .config/nvim/lua/tpe/plugins/colorscheme.lua
+++ .config/nvim/lua/tpe/plugins/colorscheme.lua
@@ -1,20 +1,18 @@
 return {
-	"slugbyte/lackluster.nvim",
-	lazy = false,
-	priority = 1000,
-	init = function()
-		require("lackluster").setup({
-			tweak_syntax = {
-				string = require("lackluster").color.luster,
-				type = "#749e94",
-			},
+	{
+		"ellisonleao/gruvbox.nvim",
+		priority = 1000,
+		config = function()
+			require("gruvbox").setup({
+				contrast = "medium", -- "hard" | "medium" | "soft"
+				palette_overrides = {},
+				transparent_mode = true,
+			})
 
-			tweak_background = {
-				normal = "none",
-			},
-		})
-		-- vim.cmd.colorscheme("lackluster")
-		vim.cmd.colorscheme("lackluster-hack") -- my favorite
-		-- vim.cmd.colorscheme("lackluster-mint")
-	end,
+			-- Set LIGHT mode
+			vim.o.background = "light"
+
+			vim.cmd.colorscheme("gruvbox")
+		end,
+	},
 }
